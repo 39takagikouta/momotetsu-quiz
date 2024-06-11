6c71b1f92817987123db2835d9de1ea5789366d8
39takagikouta <kouta.takagi0728@gmail.com>
2024-06-11 23:13:19 +0900

    トップページ作成完了

--- next/src/app/page.js
+++ next/src/app/page.js
@@ -2,23 +2,39 @@ import Link from "next/link";
 
 const Index = () => {
   return (
-    <div className='bg-red-500 min-h-screen flex flex-col items-center justify-center'>
-      <div className='text-4xl font-bold mb-6'>桃鉄クイズ</div>
-      <div className='bg-white p-6 rounded shadow-md text-center'>
-        <div className='flex flex-col space-y-4'>
-          <Link href="/country">
-            <p className='text-blue-500 hover:underline'>国クイズ</p>
-          </Link>
-          <Link href="/island">
-            <p className='text-blue-500 hover:underline'>島クイズ</p>
-          </Link>
-          <Link href="/lake">
-            <p className='text-blue-500 hover:underline'>湖クイズ</p>
-          </Link>
+    <section className="min-h-screen flex flex-col items-center justify-center bg-white">
+      <div className="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-16 max-w-7xl lg:py-24">
+        <div className="flex w-full mx-auto text-left">
+          <div className="relative inline-flex items-center mx-auto align-middle">
+            <div className="text-center">
+              <h1 className="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-neutral-600 md:text-5xl lg:text-6xl lg:max-w-7xl">
+                桃鉄クイズ
+              </h1>
+              <p className="max-w-xl mx-auto mt-8 text-base leading-relaxed text-gray-500">
+                桃太郎電鉄ワールドで、貧乏神が出してくる国旗・島・湖に関する四択クイズを練習することができます
+              </p>
+              <div className="lg:flex justify-center w-full max-w-2xl gap-2 mx-auto mt-6">
+                <div className="mt-3 rounded-lg sm:mt-0 mb-10">
+                  <Link href="/country" className="px-5 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 lg:px-10 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    国旗クイズ
+                  </Link>
+                </div>
+                <div className="mt-3 rounded-lg sm:mt-0 mb-10">
+                  <Link href="/island" className="px-7 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 lg:px-10 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    島クイズ
+                  </Link>
+                </div>
+                <div className="mt-3 rounded-lg sm:mt-0">
+                  <Link href="/lake" className="px-7 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 lg:px-10 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    湖クイズ
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <button className="btn btn-primary">Hello daisyUI!</button>
-    </div>
+    </section>
   )
 }
 
