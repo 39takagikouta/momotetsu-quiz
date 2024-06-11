889eb8967c132ccd01a5db8ee1cee7fe3258596b
39takagikouta <kouta.takagi0728@gmail.com>
2024-06-11 22:06:03 +0900

    daisyui導入

--- next/src/app/page.js
+++ next/src/app/page.js
@@ -1,20 +1,24 @@
-'use client'
-
-import useSWR from 'swr'
-import { fetcher } from '@/utils'
+import Link from "next/link";
 
 const Index = () => {
-  const url = 'http://localhost:3000/api/v1/health_check'
-  const { data, error } = useSWR(url, fetcher)
-
-  if (error) return <div>An error has occurred.</div>
-  if (!data) return <div>Loading...</div>
-
   return (
-    <>
-      <div>Rails疎通確認</div>
-      <div>レスポンスメッセージ: {data.message}</div>
-    </>
+    <div className='bg-red-500 min-h-screen flex flex-col items-center justify-center'>
+      <div className='text-4xl font-bold mb-6'>桃鉄クイズ</div>
+      <div className='bg-white p-6 rounded shadow-md text-center'>
+        <div className='flex flex-col space-y-4'>
+          <Link href="/country">
+            <p className='text-blue-500 hover:underline'>国クイズ</p>
+          </Link>
+          <Link href="/island">
+            <p className='text-blue-500 hover:underline'>島クイズ</p>
+          </Link>
+          <Link href="/lake">
+            <p className='text-blue-500 hover:underline'>湖クイズ</p>
+          </Link>
+        </div>
+      </div>
+      <button className="btn btn-primary">Hello daisyUI!</button>
+    </div>
   )
 }
 
