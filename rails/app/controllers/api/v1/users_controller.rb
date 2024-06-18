6a6a7b476f4c324117b80f31f45e7c398d5e8f19
39takagikouta <kouta.takagi0728@gmail.com>
2024-06-18 23:54:26 +0900

    修正

--- rails/app/controllers/api/v1/users_controller.rb
+++ rails/app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     class Api::V1::UsersController < ApplicationController
       def create
-        # 条件に該当するデータがあればそれを返す。なければ新規作成
         user = User.find_or_create_by!(provider: params[:provider], uid: params[:uid], name: params[:name], email: params[:email])
         if user
           head :ok
